1cdcdc12abd31472a37508a8cc99fd58dd0ba1bd
amehlhas <46384989+amehlhase316@users.noreply.github.com>
2024-10-09 11:17:31 -0700

    Update ProducerController.java

--- src/main/java/edu/asu/amexgi/rest/grocery/controllers/ProducerController.java
+++ src/main/java/edu/asu/amexgi/rest/grocery/controllers/ProducerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
